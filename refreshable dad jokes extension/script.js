ca4133e6e533ab51b0a9a7a6bb0e538d5e2c4b9d
aum <aum.davda@gmail.com>
2021-10-04 22:04:24 +0530

    I added an extensionthat not only gives a dad joke but also has a refresh button which gives a new joke every time you hit it

---
+++ refreshable dad jokes extension/script.js
@@ -0,0 +1,12 @@
+jokebutton.addEventListener("click",generatejoke)
+
+
+generatejoke();
+function generatejoke() {
+fetch('https://icanhazdadjoke.com/slack')
+    .then(data => data.json())
+    .then(jokeData => {
+        const jokeText = jokeData.attachments[0].text;
+        const jokeElement = document.getElementById('jokeElement');
+        jokeElement.innerHTML = jokeText;
+    })   }
